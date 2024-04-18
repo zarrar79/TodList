192d2a303e0ace6c51909677cfe424e271d33c6a
zarrar79 <shahzarrar79@gmail.com>
2024-04-18 22:28:33 +0500

    dropdown commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css'
+import "./App.css";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import _ from "lodash";
 import { v4 } from "uuid";
@@ -9,20 +9,21 @@ function App() {
   const [state, setState] = useState({
     todo: {
       title: "To do",
-      items: []
+      items: [],
     },
     inProgress: {
       title: "In Progress",
-      items: []
+      items: [],
     },
     done: {
       title: "Completed",
-      items: []
-    }
+      items: [],
+    },
   });
   const [input, setInput] = useState("");
+ console.log(state , "------>state")
   const handleDragEnd = ({ destination, source }) => {
-    console.log(destination,source);
+    console.log(destination, source);
     if (!destination) {
       return console.log("Not dropped inside dropable");
     }
@@ -34,7 +35,7 @@ function App() {
     }
     //Generating an copy of object
     //If we dont do this then we can't add item to other list
-    const itemCopy = {...state[source.droppableId].items[source.index]};
+    const itemCopy = { ...state[source.droppableId].items[source.index] };
     setState((prev) => {
       //Copying old state
       prev = { ...prev };
@@ -49,170 +50,24 @@ function App() {
       return prev;
     });
   };
-  const changeList = (value,index,title,status) => {
-    // Get the select element by ID
-  const selectElement = document.getElementById('mySelect');
-  // console.log(status);
-  // Get the selected option value
-  var selectedOptionValue = selectElement.value;
-  const nTask = {
-    id:v4(),
-    name : value,
-   state : status
+  const changeList = (taskIndex, sourceListKey, destinationListKey) => {
+    setState((prevState) => {
+      const newState = { ...prevState };
+      const taskToMove = newState[sourceListKey].items.splice(taskIndex, 1)[0];
+      newState[destinationListKey].items.push(taskToMove);
+      return newState;
+    });
   };
-    console.log(index, value,title,"---->");
-    console.log(selectedOptionValue);
-    if(selectedOptionValue === "Progress"){
-    setState(prevState => ({
-      ...prevState,
-      inProgress: {
-        ...prevState.inProgress,
-        items: [...prevState.inProgress.items, nTask]
-      }
-    }));
-    if(title==='To do'){
-    // Define the index of the item you want to delete
-const indexToDelete = value; // For example, let's say you want to delete the first item
 
-// Create a copy of the todo items array
-const updatedTodoItems = [...state.todo.items];
-
-// Remove the item at the specified index
-updatedTodoItems.splice(indexToDelete, 1);
-
-// Update the state with the new array
-setState(prevState => ({
-  ...prevState,
-  todo: {
-    ...prevState.todo,
-    items: updatedTodoItems
-  }
-}));
-    }
-    if(title==='Completed'){
-    // Define the index of the item you want to delete
-const indexToDelete = value; // For example, let's say you want to delete the first item
-
-// Create a copy of the todo items array
-const updatedTodoItems = [...state.done.items];
-// Remove the item at the specified index
-updatedTodoItems.splice(indexToDelete, 1);
-
-// Update the state with the new array
-setState(prevState => ({
-  ...prevState,
-  done: {
-    ...prevState.done,
-    items: updatedTodoItems
-  }
-}));
-    }
-    console.log(selectedOptionValue, title,"---> check");
-  }
-    if(selectedOptionValue === "Todo"){
-    setState(prevState => ({
-      ...prevState,
-      todo: {
-        ...prevState.todo,
-        items: [...prevState.todo.items, nTask]
-      }
-    }));
-      if(title==='In Progress'){
-      // Define the index of the item you want to delete
-  const indexToDelete = value; // For example, let's say you want to delete the first item
-  // Create a copy of the todo items array
-  const updatedTodoItems = [...state.inProgress.items];
-  
-  // Remove the item at the specified index
-  updatedTodoItems.splice(indexToDelete, 1);
-  
-  // Update the state with the new array
-  setState(prevState =>({
-    ...prevState,
-    inProgress: {
-      ...prevState.inProgress,
-      items: updatedTodoItems
-    }
-  }));
-      }
-      if(title==='Completed'){
-      // Define the index of the item you want to delete
-  const indexToDelete = value; // For example, let's say you want to delete the first item
-  
-  // Create a copy of the todo items array
-  const updatedTodoItems = [...state.done.items];
-  
-  // Remove the item at the specified index
-  updatedTodoItems.splice(indexToDelete, 1);
-  
-  // Update the state with the new array
-  setState(prevState => ({
-    ...prevState,
-    done: {
-      ...prevState.done,
-      items: updatedTodoItems
-    }
-  }));
-      }
-  }
-    if(selectedOptionValue==="Completed"){
-    setState(prevState => ({
-      ...prevState,
-      done: {
-        ...prevState.done,
-        items: [...prevState.done.items, nTask]
-      }
-    }));
-    if(title==='To do'){
-      // Define the index of the item you want to delete
-  const indexToDelete = value; // For example, let's say you want to delete the first item
-  
-  // Create a copy of the todo items array
-  const updatedTodoItems = [...state.todo.items];
-  
-  // Remove the item at the specified index
-  updatedTodoItems.splice(indexToDelete, 1);
-  
-  // Update the state with the new array
-  setState(prevState => ({
-    ...prevState,
-    todo: {
-      ...prevState.todo,
-      items: updatedTodoItems
-    }
-  }));
-      }
-      if(title==='In Progress'){
-      // Define the index of the item you want to delete
-  const indexToDelete = value; // For example, let's say you want to delete the first item
-  
-  // Create a copy of the todo items array
-  const updatedTodoItems = [...state.inProgress.items];
-  
-  // Remove the item at the specified index
-  updatedTodoItems.splice(indexToDelete, 1);
-  
-  // Update the state with the new array
-  setState(prevState => ({
-    ...prevState,
-    inProgress: {
-      ...prevState.inProgress,
-      items: updatedTodoItems
-    }
-  }));
-      }
-  }
-  }
   const addItem = () => {
-    if(input.trim()==="")
-    return;
+    if (input.trim() === "") return;
     setState((prev) => {
       return {
         ...prev,
         todo: {
           ...prev.todo,
-          items: [{ id: v4(), name: input }, ...prev.todo.items]
-        }
+          items: [{ id: v4(), name: input }, ...prev.todo.items],
+        },
       };
     });
     setInput("");
@@ -224,7 +79,8 @@ setState(prevState => ({
           e.preventDefault();
         }}
       >
-        <input className="border-[5px] border-[red] border-solid"
+        <input
+          className="border-[5px] border-[red] border-solid"
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
@@ -233,42 +89,51 @@ setState(prevState => ({
       </form>
       <div className="App">
         <DragDropContext onDragEnd={handleDragEnd}>
-          {_.map(state, (data, key) =>{
+          {_.map(state, (data, key) => {
             return (
               <div key={key} className="column">
                 <h3>{data.title}</h3>
                 <Droppable droppableId={key}>
                   {(provided) => {
                     return (
-                      <div ref = {provided.innerRef} className="droppable-col">
-                        {data.items.map((el, index) =>{
+                      <div ref={provided.innerRef} className="droppable-col">
+                        {data.items.map((el, index) => {
                           return (
                             <Draggable
                               key={el.id}
                               index={index}
-                              draggableId={el.id}>
+                              draggableId={el.id}
+                            >
                               {(provided, snapshot) => {
                                 return (
                                   <div
                                     className={`item ${
                                       snapshot.isDragging && "dragging"
                                     }`}
-                                   
                                     ref={provided.innerRef}
                                     {...provided.draggableProps}
-                                   
                                     {...provided.dragHandleProps}
                                   >
-                                    <div>
-                                    {el.name}
-                                    </div>
+                                    <div>{el.name}</div>
                                     <div className="text-black">
-                                      <select onChange={(event)=>changeList(el.name,index,data.title,event.target.value)} id="mySelect">
-                                      <option value='Todo'>Todo</option>
-                                      <option value='Progress'>In Progress</option>
-                                      <option value='Completed'>Completed</option>
+                                      <select
+                                        onChange={(e) =>
+                                          changeList(index, key, e.target.value)
+                                        }
+                                      >
+                                        {Object.keys(state).map(
+                                          (destinationList, destinationIndex) =>
+                                            destinationList !== data && (
+                                              <option
+                                                key={destinationIndex}
+                                                value={destinationList}
+                                              >
+                                                {state[destinationList].title}
+                                              </option>
+                                            )
+                                        )}
                                       </select>
-                                      </div>
+                                    </div>
                                   </div>
                                 );
                               }}
